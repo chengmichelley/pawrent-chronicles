666808351c082f6cc049b4e33acdd09e7a7a0c7d
Michelle Cheng <cheng.michelley@gmail.com>
2026-03-17 21:42:12 -0400

    Updated Cloudinary for Heroku

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,6 +2,8 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+cloudinary.config();
+
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
