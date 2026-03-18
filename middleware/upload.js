7edafb3575a47c446ac312b86939655d598e4d8b
Michelle Cheng <cheng.michelley@gmail.com>
2026-03-17 23:53:19 -0400

    UPDATED CLOUDINARY

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,9 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-cloudinary.config();
+cloudinary.config({
+	CLOUDINARY_URL: process.env.CLOUDINARY_URL
+});
 console.log("Cloudinary Config Check:", cloudinary.config().cloud_name);
 
 const storage = new CloudinaryStorage({
